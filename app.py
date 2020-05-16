15dc02e2391412fbadec9c04348fea107389dfdd
armyne <arminyepremyan@gmail.com>
2020-05-16 19:42:06 +0400

    game implementation

---
+++ app.py
@@ -0,0 +1,142 @@
+
+global board
+
+def initBoard():
+    global board
+    board = [' ' for x in range(10)]
+# initBoard
+
+def insertLetter(letter, pos):
+    board[pos] = letter
+# insertLetter
+
+def spaceIsFree(pos):
+    return board[pos] == ' '
+# spaceIsFree
+
+def printBoard():
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+    print('---------------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('---------------')
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+# printBoard
+
+def isWinner(bo, le):
+    return (bo[7] == le and bo[8] == le and bo[9] == le) or\
+    (bo[4] == le and bo[5] == le and bo[6] == le) or\
+    (bo[1] == le and bo[2] == le and bo[3] == le) or\
+    (bo[1] == le and bo[4] == le and bo[7] == le) or\
+    (bo[2] == le and bo[5] == le and bo[8] == le) or\
+    (bo[3] == le and bo[6] == le and bo[9] == le) or\
+    (bo[1] == le and bo[5] == le and bo[9] == le) or\
+    (bo[3] == le and bo[5] == le and bo[7] == le)
+# isWinner
+
+def playerMove():
+    run = True
+    while run:
+        move = input('Please select a position to place an \'X\' (1-9): ')
+        try:
+            move = int(move)
+            if move > 0 and move < 10:
+                if spaceIsFree(move):
+                    run = False
+                    insertLetter('X', move)
+                else:
+                    print('Sorry this space is occupied!')
+            else:
+                print('Please type a number within the range!')
+        except:
+            print('Please type a number')
+# playerMove
+
+
+def compMove() -> object:
+    possibleMoves = [x for x, letter in enumerate(board) if letter == ' ' and x != 0]
+    move = 0
+
+    for let in ['O', 'X']:
+        for i in possibleMoves:
+            boardCopy = board[:]
+            boardCopy[i] = let
+            if isWinner(boardCopy, let):
+                move = i
+                return move
+    cornersOpen = []
+    for i in possibleMoves:
+        if i in [1, 3, 7, 9]:
+            cornersOpen.append(i)
+    if len(cornersOpen) > 0:
+        move = selectRandom(cornersOpen)
+        return move
+
+    if 5 in possibleMoves:
+        move = 5
+        return move
+
+    edgesOpen = []
+    for i in possibleMoves:
+        if i in [2, 4, 6, 8]:
+            edgesOpen.append(i)
+    if len(edgesOpen) > 0:
+        move = selectRandom(edgesOpen)
+    return move
+# compMove
+
+def selectRandom(li):
+    import random
+    ln = len(li)
+    r = random.randrange(0, ln)
+    return li[r]
+# selectRandom
+
+def isBoardFull(board):
+    return not(board.count(' ') > 1)
+# isBoardFull
+
+def main():
+    print('Welcome to Tic Tac Toe!')
+    printBoard()
+
+    while not(isBoardFull(board)):
+        if not(isWinner(board, 'O')):
+            playerMove()
+            printBoard()
+        else:
+            print('Sorry, O\'s won this time')
+            break
+
+        if not(isWinner(board, 'X')):
+            move = compMove()
+            if move == 0:
+                print('Tie Game!')
+            else:
+                insertLetter('O', move)
+                print('Computer placed an \'O\' in position', move, ':')
+                printBoard()
+        else:
+            print('X\'s won this time! Good Job!')
+            break
+
+    if isBoardFull(board):
+        print('Tie Game!')
+# main
+
+if __name__ == '__main__':
+    while True:
+        answer = input('Do you want to play again? (Y/N)')
+        if answer.lower() == 'y' or answer.lower == 'yes':
+            initBoard()
+            print('-----------------------------------')
+            main()
+        elif answer.lower() == 'n' or answer.lower == 'no':
+            break
+        else:
+            answer = input('Do you want to play again? (Y/N)')
